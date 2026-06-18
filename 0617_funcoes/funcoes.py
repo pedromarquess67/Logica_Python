dfa5bcddb4e4447bf4aad39f343850dbc5f46de0
PedroMarques <pepoaimessi@gmail.com>
2026-06-17 22:04:35 -0300

    função para somar valores

---
+++ 0617_funcoes/funcoes.py
@@ -0,0 +1,7 @@
+def somar_valores (parametro1, parametro2):
+    """Docstring: descreve o que a função faz"""
+    #corpo da funcao
+    resultado= parametro1 + parametro2
+    return resultado
+soma= somar_valores(6, 7)
+print(soma)
