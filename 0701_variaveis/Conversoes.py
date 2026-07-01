001c20fa5aacf7b3552e587b77f845168c94dff6
PedroMarques <pepoaimessi@gmail.com>
2026-07-01 20:34:26 -0300

    conversoes

---
+++ 0701_variaveis/Conversoes.py
@@ -0,0 +1,11 @@
+def celsius_fahrenheit(c:float)-> float:
+    """Converte temperatura em Celsius para Fahrenheit.
+    Fórmula: F=C*9/5 + 32
+    """
+    return c*9/5 + 32
+
+def metros_quilometros(m:float)-> float:
+    """Converte distancia em metros para quilometros
+    1 km =1000 m
+    """
+    return m/1000
